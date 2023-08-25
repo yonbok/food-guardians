cfbd1de7c9ddd879165a2e8c4919a439f4ecc29a
yonbok <na.re.zutomayo14@gmail.com>
2023-08-25 09:29:45 +0000

    販売停止中に変更できない

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,13 +17,13 @@ class Admin::CustomersController < ApplicationController
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
     else
-      render :edit
+      render :show
     end
   end
-  
-  
+
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:name, :kana_name, :email, :post_code, :address, :phone_number, :is_deleted)
   end
