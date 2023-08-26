2f230b1b84d72cecd296131c48523157aadd7c23
yonbok <na.re.zutomayo14@gmail.com>
2023-08-26 15:20:01 +0000

    enum機能編集したが日本語化されない

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
 
-  enum payment: { credit_card: 0, cod: 1}
+  enum payment_method: { credit_card: 0, cod: 1}
   enum order_status: { waiting_for_payment: 0, payment_confirmed: 1, in_production: 2, preparing_to_ship: 3, shipped: 4 }
 
   def self.order_statuses_i18n
