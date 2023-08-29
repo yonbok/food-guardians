ee312196138ea39b3d24c7792446f0e1fe454efb
yonbok <na.re.zutomayo14@gmail.com>
2023-08-29 11:15:30 +0000

    genre

--- db/migrate/20230826044359_add_genre_to_items.rb
+++ db/migrate/20230826044359_add_genre_to_items.rb
@@ -1,6 +1,5 @@
 class AddGenreToItems < ActiveRecord::Migration[6.1]
   def change
     add_column :items, :genre_id, :integer, null: false
-    add_foreign_key :items, :genres
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -145,5 +145,4 @@ ActiveRecord::Schema.define(version: 2023_08_26_044359) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "genres"
 end
