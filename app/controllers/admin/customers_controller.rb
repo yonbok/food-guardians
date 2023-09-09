003c59e78a105ab1ff493bf2acab420c3c7fcc8e
yonbok <na.re.zutomayo14@gmail.com>
2023-09-09 02:12:43 +0000

    購入者側会員ステータス正常に作動

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,7 +17,7 @@ class Admin::CustomersController < ApplicationController
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
     else
-      render :show
+      render :edit
     end
   end
 
